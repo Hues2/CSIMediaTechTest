4c8127ec82067fc4873d9ed36dd0563701b50e8c
Greg Ross <gregandrewross79@gmail.com>
2022-12-08 20:43:04 +0000

    - Created the Create controller - There are now 2 pages (Home & Create) - The home view will show the list of all saved inputs, and the create page will allow the user to create a new input

---
+++ CSIMediaTechTest/Controllers/CreateController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSIMediaTechTest.Controllers
+{
+    public class CreateController : Controller
+    {
+
+        private readonly ILogger<CreateController> _logger;
+
+        public CreateController(ILogger<CreateController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CSIMediaTechTest/Views/Create/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSIMediaTechTest.Views.Create
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
